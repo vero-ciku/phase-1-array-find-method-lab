b7ec4a80fdc89686066a9bf91cbca8ea35cfc4cf
Veronicah <veronicah.gicheru@student.moringaschool.com>
2022-06-09 16:29:05 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin (array) {
+    const result =array.find (array =>(array.result=== 'W'));
+    if (result === undefined) {
+        return undefined;
+    }
+else {
+    return result.year
+}
+}
